9188aee11cb4ba488d932246fcf0479d95060c0e
Faris Chebib <faris@theluckybead.com>
2018-09-04 15:50:26 -0600

    Initial commit

---
+++ utah/slcpython/__init__.py
@@ -0,0 +1,9 @@
+def howdy():
+    msg = """
+        Howdy, Salt Lake City!
+        "Our next meetup is on:"
+        "Wednesday, September 5th, at 6:30 p.m."
+        "and is about:"
+        "A trio of python talks: Beginning / Intermediate / Advanced"
+        """
+    print(msg)
